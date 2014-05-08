5117e52af3775caf939b1e50fdf28a19327765b5
Mangesh Paranjape <mangeshp16@gmail.com>
2014-05-07 22:45:38 -0400

    TDV
    TDV

---
+++ NJTDVAPI/Models/DV.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace NJTDVAPI.Models
+{
+    public class DV
+    {
+        public string GetDVByStationID(string stationID)
+        {
+            HttpWebResponse response;
+            string dv=string.Empty;
+
+            if (Request_dv_njtransit_com(out response,stationID ))
+            {
+                Stream st = response.GetResponseStream();
+                StreamReader sr = new StreamReader(st);
+                dv = sr.ReadToEnd();
+                response.Close();
+            }
+            return dv;
+        }
+
+        private bool Request_dv_njtransit_com(out HttpWebResponse response,string stationID)
+        {
+            response = null;
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://dv.njtransit.com/mobile/tid-mobile.aspx?sid=" + stationID );
+
+                request.UserAgent = "Fiddler";
+
+                response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (WebException e)
+            {
+                if (e.Status == WebExceptionStatus.ProtocolError) response = (HttpWebResponse)e.Response;
+                else return false;
+            }
+            catch (Exception)
+            {
+                if (response != null) response.Close();
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ NJTDVAPI/obj/Debug/Package/PackageTmp/Controllers/DVController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NJTDVAPI.Models;
+
+namespace NJTDVAPI.Controllers
+{
+    public class DVController : ApiController
+    {
+        // GET api/dv
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/dv/5
+        public string Get(string id)
+        {
+            string dv = string.Empty;
+            DV objDV = new DV();
+            dv = objDV.GetDVByStationID(id);
+            return dv;
+        }
+
+        // POST api/dv
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/dv/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/dv/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
